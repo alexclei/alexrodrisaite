5a217c771e9257307ce99c1ef64845e5da659ea0
Alex Rodrigeus <alexcleiton16@gmail.com>
2020-06-22 00:39:39 -0300

    Add Model e Migração de Portfolio

---
+++ database/migrations/2020_06_20_022201_create_portfolios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->string('imagem');
+            // $table->unsignedBigInteger('cat_id');
+            // $table->foreign('cat_id')->references('id')->on('categoria')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
