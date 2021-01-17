5964c35db96e0d38e2c74bcb3163f14391d0a69f
Alex Rodrigues <alexcleiton16@gmail.com>
2021-01-17 13:13:06 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Spatie\Sitemap\SitemapGenerator;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,22 +12,35 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+if (Request::server('HTTP_HOST') != 'localhost:8000')
+{
+    // URL::forceScheme('https');
+}
+
+
 Route::namespace('Site')->group( function(){
     Route::get('/','SiteController@inicial')->name('init');
     Route::get('/sobre','SiteController@sobre')->name('sobre');
     Route::get('/servicos','SiteController@servicos')->name('servico');
+    Route::get('/servicos/{id}/{name}','SiteController@servicosShow')->name('servicoshow');
+    Route::post('/boleto_action', 'PagSeguroController@action_boletoPGS')->name('boleto_action');
     Route::get('/portfolio','SiteController@portfolio')->name('portfolio');
     Route::get('/blog','SiteController@blog')->name('blog');
     Route::get('/blog/{id}/{name}','SiteController@post')->name('post');
     Route::get('/contato','SiteController@contato')->name('contato');
-    
+    Route::get('/obrigado','SiteController@contatoOk')->name('lead-ok');
+    Route::post('/contato','SiteController@enviarContato')->name('enviarcontato');
     /*
     Route::get('/equipe','SiteController@index')->name('equipe');
     Route::get('/servicos/criacao-de-site','SiteController@index')->name('site');
     */
 });
 
-Route::get('/updatesitemap','SitemapController@sitemap')->name('sitemap');
+Route::get('a', function () {
+    Artisan::call('sitemap:generator', [] );
+});
+
 Auth::routes();
 
 Route::namespace('Admin')->group(function () {
@@ -33,9 +48,9 @@ Route::namespace('Admin')->group(function () {
         Route::get('/painel', 'HomeController@index')->name('home');
         Route::prefix('painel')->group(function () {
             Route::resource('portfolio', 'PortfolioController');
-            // Route::resource('portfolio', 'PortfolioController');
-            // Route::resource('post', 'PortfolioController');
-            // Route::resource('banner', 'PortfolioController');
+            Route::resource('contato', 'ContatoController');
+            Route::resource('post', 'PostController');
+            Route::resource('servico', 'ServicoController');
         });
     });
 });
