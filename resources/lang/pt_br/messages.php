9371f1907552e2037d516f5c41485b5af76715f8
Alex Rodrigues <alexcleiton16@gmail.com>
2021-01-23 12:05:12 -0300

    Tradução

---
+++ resources/lang/pt_br/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tradução do sistema em portugues
+    |--------------------------------------------------------------------------
+    |
+    | Terá todo o sistema de usuario e admin traduzido em portugues do Brasil
+    | (pt_BR), posteriormente as demais areas do sistema.
+    |
+    */
+
+    'login' => [
+        'title' => 'Logue em sua conta!',
+        'email' => 'Email, Telefone ou CPF/CNPJ',
+        'password' => 'Senha',
+        'login' => 'Entrar',
+        'rememberme' => 'Lembrar-Me',
+        'forgotpassword' => 'Esqueceu sua senha?',
+    ]
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,16 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">@lang('messages.login.title')</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
+                        <div class="form-group row justify-content-center">
+                            
                             <div class="col-md-6">
+                                <label for="email" class="col-form-label text-md-right">@lang('messages.login.email')</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -25,10 +25,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
+                        <div class="form-group row justify-content-center">
+                            
                             <div class="col-md-6">
+                                <label for="password" class="col-form-label text-md-right">@lang('messages.login.password')</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -45,7 +45,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        @lang('messages.login.rememberme')
                                     </label>
                                 </div>
                             </div>
@@ -54,12 +54,12 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    @lang('messages.login.login')
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        @lang('messages.login.forgotpassword')
                                     </a>
                                 @endif
                             </div>
