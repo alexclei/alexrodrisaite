38c19d9e282cb7e1fe3e0967a9b5aa39c7c29891
Alex Rodrigues <alexcleiton16@gmail.com>
2021-02-22 10:50:23 -0300

    UPDATE DB

---
+++ database/migrations/2021_02_19_102448_create_orcamentos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrcamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orcamentos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('email');
+            $table->string('conheceu');
+            $table->string('telefone', 20);
+            $table->string('assunto');
+            $table->text('mensagem');
+            $table->text('resposta')->nullable();
+            $table->unsignedBigInteger('servico_id');
+            $table->foreign('servico_id')->references('id')->on('servicos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contatos');
+    }
+}
